dbcf2bbcb7163300c856fdae8985fd7602fce043
SIGMAINFOAHD\nilesh.p <nilesh.p@sigmainfo.net>
2017-05-24 13:02:43 +0530

    Login page and registration page integration

---
+++ src/DMS.WebAppUI/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { EqualValidator } from '../shared/appequal-validator.directive';
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [EqualValidator],
+    exports: [
+        CommonModule,
+        FormsModule
+
+    ]
+})
+export class SharedModule { }
