6f16cb9cec4f39a87c3a10d245fb37da50248cd4
SIGMAINFOAHD\nilesh.p <nilesh.p@sigmainfo.net>
2017-05-26 11:07:53 +0530

    Reset Password modal implemted

--- src/DMS.WebAppUI/src/app/shared/shared.module.ts
+++ src/DMS.WebAppUI/src/app/shared/shared.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { EqualValidator } from '../shared/appequal-validator.directive';
 //import { DataTableModule } from '../angular2-datatable/datatable.module';
 import { DataTableModule } from "angular2-datatable";
-
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 @NgModule({
     imports: [
+        Ng2Bs3ModalModule,
         CommonModule
     ],
     declarations: [EqualValidator],
     exports: [
         CommonModule,
-
+        Ng2Bs3ModalModule,
         DataTableModule,
         FormsModule,
 
