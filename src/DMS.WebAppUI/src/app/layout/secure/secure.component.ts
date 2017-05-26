3e9911f4d73111ea117fdbb19d52d87e87afb29e
SIGMAINFOAHD\nilesh.p <nilesh.p@sigmainfo.net>
2017-05-26 18:36:34 +0530

    conflict resolved

--- src/DMS.WebAppUI/src/app/layout/secure/secure.component.ts
+++ src/DMS.WebAppUI/src/app/layout/secure/secure.component.ts
@@ -1,25 +1,18 @@
 ﻿import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import 'rxjs/Rx';
-import { Subscription } from 'rxjs';
-import { Routes, RouterModule } from '@angular/router';
 import { IUser, User } from '../../login/login';
 import { Router, ActivatedRoute } from '@angular/router';
-=======
 import { NotificationsService, SimpleNotificationsComponent, PushNotificationsService } from 'angular2-notifications';
 import 'rxjs/Rx';
 import { Subscription } from 'rxjs';
 import { Routes, RouterModule } from '@angular/router';
 import { GlobalVariable } from '../../shared/global';
-
->>>>>>> e9990789cc69124ca9e5ae7c620086ad671a70cc
 @Component({
     selector: 'app-secure',
     templateUrl: './secure.component.html',
     providers: [NotificationsService]
 })
 export class SecureComponent {
-<<<<<<< HEAD
     private currentUser: IUser;
 
     constructor(
@@ -38,9 +31,6 @@ export class SecureComponent {
         localStorage.removeItem('currentUser');
         this.router.navigate(['/login']);
     }
-=======
     public notificationOptions = GlobalVariable.notificationOptions;
-
->>>>>>> e9990789cc69124ca9e5ae7c620086ad671a70cc
 }
 
