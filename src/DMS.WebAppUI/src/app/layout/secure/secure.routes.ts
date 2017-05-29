4af88b6ac9fa946a24bf35377869b4b485e8c3d1
SIGMAINFOAHD\shreya.p <shreya.p@sigmainfo.net>
2017-05-29 11:58:47 +0530

    Rights screen

--- src/DMS.WebAppUI/src/app/layout/secure/secure.routes.ts
+++ src/DMS.WebAppUI/src/app/layout/secure/secure.routes.ts
@@ -5,12 +5,14 @@ import { AccessHistoryComponent } from '../../accesshistory/accesshistory.compon
 import { AuthGuard } from '../../shared/auth.guard';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { RolesComponent } from '../../roles/roles.component';
+import { RightsComponent } from '../../roles/rights.component';
 
 export const SECURE_ROUTES: Routes = [
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
     { path: 'userprofile', component: UserProfileComponent, canActivate: [AuthGuard] },
     { path: 'document', component: DocumentComponent, canActivate: [AuthGuard] },
     { path: 'accesshistory', component: AccessHistoryComponent, canActivate: [AuthGuard] },
-    { path: 'roles', component: RolesComponent, canActivate: [AuthGuard] }
+    { path: 'roles', component: RolesComponent, canActivate: [AuthGuard] },
+    { path: 'rights', component: RightsComponent, canActivate: [AuthGuard] }
 
 ];
