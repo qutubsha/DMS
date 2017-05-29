0c191181b7d78ec72a2df299d343ad6e18db38be
SIGMAINFOAHD\shreya.p <shreya.p@sigmainfo.net>
2017-05-29 12:40:47 +0530

    App settings

--- src/DMS.WebAppUI/src/app/appSettings.ts
+++ src/DMS.WebAppUI/src/app/appSettings.ts
@@ -1,6 +1,6 @@
 ﻿export class AppSettings {
     
-    public static get _DMSUrl(): string { return 'http://localhost:5000'; }
+    public static get _DMSUrl(): string { return 'http://localhost:56833/api'; }
    // public static get _DMSUrl(): string { return 'http://localhost:9091/api'; }
     //public static get _DMSUrl(): string { return 'http://111.93.247.122:9010/HRMSWebAPI/api'; }
     //public static get _DMSUrl(): string { return 'http://172.16.1.22:9091/api'; }
