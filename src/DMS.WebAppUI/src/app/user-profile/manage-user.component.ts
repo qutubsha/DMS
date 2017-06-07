adcd20ea3b1efe2325d1a09058c79935595b61e9
SIGMAINFOAHD\nilesh.p <nilesh.p@sigmainfo.net>
2017-06-07 14:15:33 +0530

    Manage User List Page Implemented

--- src/DMS.WebAppUI/src/app/user-profile/manage-user.component.ts
+++ src/DMS.WebAppUI/src/app/user-profile/manage-user.component.ts
@@ -1,40 +1,56 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { IUserRegistration, UserRegistration, IUser} from '../login/login';
+import { IUserRegistration, UserRegistration, IUser, IUserDetails} from '../login/login';
 import 'rxjs/Rx';
 import { Subscription } from 'rxjs';
 import { UserService } from '../services/user.service';
+import { DataTable } from "angular2-datatable";
+import { GlobalVariable, IDictionary } from '../shared/global';
 @Component({
     templateUrl: './manage-user.component.html',
 })
 export class UserComponent {
-    private userprofile: IUserRegistration;
+    private userdetailsdata: IUserDetails[];
+    private userdetails: IUserDetails;
     private notificationTitle: string = '';
     private notificationContent: string = '';
     private errorMessage: string;
     busy: Subscription;
+    private data: IUserDetails[];
     private currentUser: IUserRegistration;
+    private rowsOnPage = GlobalVariable.rowsOnPage;
+    private sortBy = 'Email';
+    private sortOrder = 'asc';
+    private activePage = 1;
+    @ViewChild('mf') mf: DataTable;
     constructor(
         private router: Router,
         private _userService: UserService
     ) { }
 
     ngOnInit(){
-        this.userprofile = {
-            UserId: 0,
-            UserName: '',
-            Password: '',
-            RepeatPassword: '',
-            FirstName: '',
-            LastName: '',
-            Email: '',
-        };
-       
+        //this.userdetailsdata= {
+        //     ,
+        //    Password: '',
+        //    FirstName: '',
+        //    LastName: '',
+        //    Email: '',
+        //};
+        this.GetUserdetails();
     }
 
     redirectToDashbord() {
         this.router.navigate(['/dashboard']);
     }
 
+
+    GetUserdetails() {
+        this.busy = this._userService.getuserlist()
+            .subscribe(data => {
+                this.userdetailsdata = data;
+               this.data = data;
+            },
+            error => this.errorMessage = <any>error);
+    }
 
 }
